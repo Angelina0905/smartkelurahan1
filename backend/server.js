1a6b0a39c5f73349ab6cf33b15fddb1c52c93a6f
Angelina0905 <yudriangell@gmail>
2026-05-16 08:11:56 +0700

    fix mysql ssl

--- backend/server.js
+++ backend/server.js
@@ -33,14 +33,11 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-
-  ssl: {
-    rejectUnauthorized: false,
-  },
 });
 
 db.connect((err) => {
   if (err) {
+    console.log("MySQL Error 😭🔥");
     console.log(err);
   } else {
     console.log("MySQL Connected 😭🔥");
